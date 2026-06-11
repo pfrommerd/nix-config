9072d4287102d64be37d64140be925bd6a0c124b
Daniel Pfrommer <dan.pfrommer@gmail.com>
2026-06-11 18:21:59 -0400

    Added cosmic-term patch to disable ctrl+shift keys from being captured

--- overlay.nix
+++ overlay.nix
@@ -1,11 +1,15 @@
 {
   nixpkgs,
   ...
-}: final: prev: {
-  code-cursor = (final.callPackage ./pkgs/code-cursor {});
-  cursor-cli = (final.callPackage ./pkgs/cursor-cli {});
-  pi-coding-agent = (final.callPackage ./pkgs/pi-coding-agent {});
-  zed-editor = (final.callPackage ./pkgs/zed-editor {});
+}:
+final: prev: {
+  code-cursor = (final.callPackage ./pkgs/code-cursor { });
+  cursor-cli = (final.callPackage ./pkgs/cursor-cli { });
+  pi-coding-agent = (final.callPackage ./pkgs/pi-coding-agent { });
+  zed-editor = (final.callPackage ./pkgs/zed-editor { });
+  cosmic-term = prev.cosmic-term.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [ ./pkgs/cosmic-term/pass-ctrl-shift-to-terminal.patch ];
+  });
   # patched widevine for aarch64
-  widevine-cdm = (final.callPackage ./pkgs/widevine-cdm {});
+  widevine-cdm = (final.callPackage ./pkgs/widevine-cdm { });
 }
