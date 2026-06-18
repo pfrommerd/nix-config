6ba3043ab5ff7965e3031070991d2faef97aa97f
Daniel Pfrommer <dan.pfrommer@gmail.com>
2026-06-18 02:41:56 -0400

    Added scrolling patch

--- overlay.nix
+++ overlay.nix
@@ -8,7 +8,10 @@ final: prev: {
   pi-coding-agent = (final.callPackage ./pkgs/pi-coding-agent { });
   zed-editor = (final.callPackage ./pkgs/zed-editor { });
   cosmic-term = prev.cosmic-term.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [ ./pkgs/cosmic-term/pass-ctrl-shift-to-terminal.patch ];
+    patches = (old.patches or [ ]) ++ [
+      ./pkgs/cosmic-term/pass-ctrl-shift-to-terminal.patch
+      ./pkgs/cosmic-term/fractional-wheel-scrolling.patch
+    ];
   });
   # patched widevine for aarch64
   widevine-cdm = (final.callPackage ./pkgs/widevine-cdm { });
